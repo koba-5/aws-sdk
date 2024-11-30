0e7230df9c3cbe8f73b1317896046a903ea80310
koba-5 <s-kobata.ju@outlook.com>
2024-11-30 11:25:29 +0900

    'first'

---
+++ README.md
@@ -0,0 +1,5 @@
+# aws-sdk-sample
+
+## introdaction
+
+このリポジトリは、aws-sdk を用いたサンプル実装を行うためのもの。
---
+++ web-api/src/test/kotlin/aws_sdk/ApplicationTests.kt
@@ -0,0 +1,10 @@
+package aws_sdk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test fun contextLoads() {}
+}
